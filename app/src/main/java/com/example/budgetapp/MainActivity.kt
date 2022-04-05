9ed68c0da7fbbddb416ba3980dafeb96beb96cec
szapata726 <szapata@gustavus.edu>
2022-04-05 14:11:57 -0500

    Created basic UI for the main budget page

--- app/src/main/java/com/example/budgetapp/MainActivity.kt
+++ app/src/main/java/com/example/budgetapp/MainActivity.kt
@@ -5,24 +5,36 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
-private lateinit var addButton: Button
+private lateinit var paycheckAddButton: Button
+private lateinit var billAddButton: Button
 private lateinit var viewExpense: Button
-private lateinit var amountInput: EditText
-private lateinit var messageInput: EditText
-private lateinit var dateInput: EditText
+private lateinit var paycheckAmount: EditText
+private lateinit var billAmount: EditText
+private lateinit var jobInput: EditText
+private lateinit var billTitle: EditText
+private lateinit var dateBill: EditText
+private lateinit var datePaycheck: EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addButton = findViewById(R.id.add_button)
+        paycheckAddButton = findViewById(R.id.add_paycheck_button)
+        billAddButton = findViewById(R.id.add_bill_button)
         viewExpense = findViewById(R.id.view_expense_button)
-        amountInput = findViewById(R.id.amount_input)
-        messageInput = findViewById(R.id.message_input)
-        dateInput = findViewById(R.id.date_input)
+        paycheckAmount = findViewById(R.id.paycheck_amount)
+        billAmount = findViewById(R.id.bill_amount)
+        jobInput = findViewById(R.id.job_input)
+        billTitle = findViewById(R.id.bill_title)
+        dateBill = findViewById(R.id.date_input)
+        datePaycheck = findViewById(R.id.date1_input)
 
-        addButton.setOnClickListener {
-            // save in database for amount and message
+        paycheckAddButton.setOnClickListener {
+            // save in database for amount, date, and job
+        }
+        billAddButton.setOnClickListener {
+            // save in database for amount, date, and bill title
         }
         viewExpense.setOnClickListener {
             // start new activity
