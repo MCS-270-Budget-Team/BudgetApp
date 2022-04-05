9a52ea04e12c1937020432192ba6d2cc7bf9547b
hvrlxy <hle5@gustavus.edu>
2022-04-05 14:10:20 -0500

    'add sorting by date in expenseDB'

--- app/src/main/java/com/example/budgetapp/MainActivity.kt
+++ app/src/main/java/com/example/budgetapp/MainActivity.kt
@@ -10,17 +10,22 @@ class MainActivity : AppCompatActivity() {
 
         val dbHelper = ExpenseDB(this)
 
-        val expense = Expense(null, "groceries", "04/04/2022", 100.5, "others")
+        val expense1 = Expense(null, "groceries", "04/04/2022", 100.5, "others")
+        val expense2 = Expense(null, "coffee", "04/052022", 30.6, "others")
+        val expense3 = Expense(null, "gas", "04/03/2022", 35.2, "others")
+
         dbHelper.deleteAllData()
-        dbHelper.insertData(expense)
+        dbHelper.insertData(expense1)
+        dbHelper.insertData(expense2)
+        dbHelper.insertData(expense3)
         var expenseList = dbHelper.readData()
 
-        val id = expenseList[0].id
-        val new_expense = Expense(null, "cafe", "04/05/2022", 20.0, "others")
+//        val id = expenseList[0].id
+//        val new_expense = Expense(null, "cafe", "04/05/2022", 20.0, "others")
+//
+//        dbHelper.updateData(id, new_expense)
 
-        dbHelper.updateData(id, new_expense)
-
-        expenseList = dbHelper.readData()
+//        expenseList = dbHelper.readData()
         print(expenseList)
     }
 }
