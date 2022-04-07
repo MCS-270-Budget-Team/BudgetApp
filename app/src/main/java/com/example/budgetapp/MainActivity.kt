66fe743e4b759bcb596eca09cf365ed1a137bee6
szapata726 <szapata@gustavus.edu>
2022-04-07 13:07:18 -0500

    new updates to main

--- app/src/main/java/com/example/budgetapp/MainActivity.kt
+++ app/src/main/java/com/example/budgetapp/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.budgetapp
 
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +25,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var billAmount: EditText
     private lateinit var jobInput: EditText
     private lateinit var billTitle: EditText
-    private lateinit var dateBill: EditText
+    private lateinit var dateBill: TextView
     private lateinit var datePaycheck: EditText
+
+    private lateinit var dateButton: Button
+    var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +43,33 @@ class MainActivity : AppCompatActivity() {
         billTitle = findViewById(R.id.bill_title)
         dateBill = findViewById(R.id.date_input)
         datePaycheck = findViewById(R.id.date1_input)
+        dateButton = findViewById(R.id.date_picker)
+
+        dateBill!!.text = "--/--/----"
 
         val context = this
         val db = ExpenseDB(context)
+
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(p0: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        dateButton!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@MainActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+        })
 
 
         paycheckAddButton.setOnClickListener {
@@ -59,5 +92,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, ExpenseViewer::class.java) //
             startActivity(intent)
         }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        dateBill!!.text = sdf.format(cal.getTime())
     }
 }
