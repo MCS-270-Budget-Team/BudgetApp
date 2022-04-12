c56e4c47884b7e294e2dcb1330df380c0df2543b
Junming Wu <68437466+junwu168@users.noreply.github.com>
2022-04-12 18:32:32 -0500

    Revert "add table Recurring Bill and relative functions"

--- app/src/main/java/com/example/budgetapp/Distribute.kt
+++ app/src/main/java/com/example/budgetapp/Distribute.kt
@@ -2,5 +2,5 @@ package com.example.budgetapp
 
 data class Distribute( var id: Int? = null,
                        var category: String = "None",
-                       var percentage: Double = 0.0,
+                       var percentage: Double = 0.1,
                        var max_amount: Double = 100.1)
--- app/src/main/java/com/example/budgetapp/MainActivity.kt
+++ app/src/main/java/com/example/budgetapp/MainActivity.kt
@@ -28,8 +28,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var date1Button: Button
     var cal = Calendar.getInstance()
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -50,7 +48,6 @@ class MainActivity : AppCompatActivity() {
 
         val context = this
         val db = EntriesDB(context)
-
 
         val dateSetListener = object : DatePickerDialog.OnDateSetListener {
             override fun onDateSet(p0: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
@@ -127,56 +124,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, ExpenseViewer::class.java) //
             startActivity(intent)
         }
-
-        /***************************TEST*************************************************/
-//        val d1 = Distribute(null, "Transportation", 0.2, 200.0)
-//        val d2 = Distribute(null, "Food", 0.3, 250.0)
-//        val d3 = Distribute(null, "Food", 0.2, 300.0)
-//        val d4 = Distribute(null, "Entertain", 0.2, 200.0)
-//        val d5 = Distribute(null, "Other", 0.2, 200.0)
-//        db.deleteAll_Distribute()
-//        val row1 = db.insert_Distribute(d1)
-//        val row2 = db.insert_Distribute(d2)
-//        val row3 = db.insert_Distribute(d3)
-//        val row4 = db.insert_Distribute(d4)
-//        val row5 = db.insert_Distribute(d5)
-//
-//
-//        val myRow3 = db.getRow_Distribute(3)
-//        db.updateRow_Distribute(1, Distribute(null, "Family", 0.2, 200.0))
-//
-//        val category = db.getCategories_Distribute()
-//
-//        val distributes = db.getAll_Distribute()
-
-        val c1 = RecurringExpense(null, "Netflix", 39.9, "04/16/2022", "Entertain",
-                                    "03/16/2022", false)
-        val c2 = RecurringExpense(null, "Amazon", 29.9, "04/20/2022", "Living",
-            "03/20/2022", false)
-
-        val c3 = RecurringExpense(null, "Spotify", 9.9, "04/22/2022", "Entertain",
-            "03/19/2022", true)
-        db.deleteAll_Recurring()
-        val row1 = db.insert_Recurring(c1)
-        val row2 = db.insert_Recurring(c2)
-        val row3 = db.insert_Recurring(c3)
-
-        val result1 = db.getAll_Recurring()
-
-        val new_c2 = RecurringExpense(null, "Amazon", 29.9, "04/30/2022", "Living",
-            "03/20/2022", true)
-
-        val new_row2 = db.updateRow_Recurring(2, new_c2)
-        val result2 = db.getAll_Recurring()
-
-        db.deleteRow_Recurring(1)
-        val result3 = db.getAll_Recurring()
-
-
-
-        println("nothing")
-
-
     }
     // end of onCreate
 
@@ -190,5 +137,4 @@ class MainActivity : AppCompatActivity() {
         val sdf = SimpleDateFormat(myFormat, Locale.US)
         datePaycheck!!.text = sdf.format(cal.getTime())
     }
-
 }
